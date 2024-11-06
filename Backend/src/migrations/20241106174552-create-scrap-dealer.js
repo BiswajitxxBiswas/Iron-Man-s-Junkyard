a6a779b297f82a5a05e62560856a0100dd01e563
raghavmanish <mnsyd24@gmail.com>
2024-11-07 00:17:32 +0530

    Added Users , ScrapDealers , ScrapRequests models and creted one to many associationn between User to ScrapRequest and ScrapDealer to ScrapRequest

--- Backend/src/migrations/20241106174552-create-scrap-dealer.js
+++ Backend/src/migrations/20241106174552-create-scrap-dealer.js
@@ -2,28 +2,33 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('ScrapDealers', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
       email: {
-        type: Sequelize.STRING , 
-        allowNull : false ,
-        unique : true ,
-        validate : {
-          isEmail : true ,
-        }
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
       },
       password: {
-        type: Sequelize.STRING ,
-        allowNull : false  ,
-        validate : {
-          len : [3 , 50] ,
-        }
+        type: Sequelize.STRING,
+        allowNull: false,
       },
+      operationalLocations: {
+        type: Sequelize.STRING, // Store locations as JSONB (array of strings)
+        allowNull: true,
+      },
+      status : {
+        type : Sequelize.STRING ,
+      } ,
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
@@ -35,6 +40,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('ScrapDealers');
   }
 };
